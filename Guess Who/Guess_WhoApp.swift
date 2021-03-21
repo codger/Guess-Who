1ee2283848fe57afd20bda79b76577eeb6fd77b6
John James <jjames@pacbell.net>
2021-03-20 22:22:52 -0700

    Initial Commit

---
+++ Guess Who/Guess_WhoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Guess_WhoApp.swift
+//  Guess Who
+//
+//  Created by John James on 3/20/21.
+//
+
+import SwiftUI
+
+@main
+struct Guess_WhoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
